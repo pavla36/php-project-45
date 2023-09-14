093ea2bf34facb36e64e912ca19c28031bdc4c31
pavla36 <palshina.anyaa@gmail.com>
2023-09-14 18:48:11 +0300

    t

--- src/GcdGame.php
+++ src/GcdGame.php
@@ -29,7 +29,7 @@ function gcd()
                 }
                 line("Correct!");
                 break;
-            }
+            } 
         }
     }
     line("Congratulations, {$name}!");
