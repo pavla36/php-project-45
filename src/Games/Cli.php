d1fed5105d9523a9bc42e4bfd90577bdc15bfc9a
pavla36 <palshina.anyaa@gmail.com>
2023-09-16 22:41:14 +0300

    fix

--- src/Games/Cli.php
+++ src/Games/Cli.php
@@ -8,6 +8,6 @@ use function cli\prompt;
 function greeting()
 {
     line('Welcome to the Brain Games!');
-    // $name = prompt('May I have your name?');
-    // line("Hello, %s!", $name);
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
